f3f4dca39e781ac41a1d7b4f325fb41055e1d3f4
shirai <ryhshr3@gmail.com>
2025-04-22 01:16:33 +0900

    feat: NijiVoice APIを呼び出すfetchNijiVoice関数を追加し、main関数を非同期に変更。環境変数のキー名を修正。

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import { getEnvVar } from "./lib/env.js"
+import { fetchNijiVoice } from "./lib/fetch.js"
 
-export function main(): void {
-  const { nijivoiceApiKey } = getEnvVar()
-  console.log(nijivoiceApiKey)
+async function main(): Promise<void> {
+  const response = await fetchNijiVoice({
+    actorId: "90031163-c497-44f3-a8a6-e45e4d0cb8f6",
+    word: "こんにちは！",
+    speed: 0.8,
+  })
+
+  console.log(response)
 }
 
 main()
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,12 +3,12 @@ import { config } from "dotenv"
 config()
 
 interface EnvVar {
-  nijivoiceApiKey: string
+  nijiVoiceApiKey: string
 }
 
-export const getEnvVar = (): EnvVar => {
+export function getEnvVar(): EnvVar {
   const vars: Partial<EnvVar> = {
-    nijivoiceApiKey: process.env.NIJIVOICE_API_KEY,
+    nijiVoiceApiKey: process.env.NIJI_VOICE_API_KEY,
   }
 
   for (const [key, value] of Object.entries(vars)) {
