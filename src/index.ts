56b8676f965322a2eb4f2b4a723559f08fe2b396
shirai <ryhshr3@gmail.com>
2025-04-22 11:33:40 +0900

    feat: actors.tsを新規作成し、getActorId関数を修正してactorIdの取得を整理。main関数の呼び出しを整形。

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { fetchNijiVoice } from "./lib/nijiVoice/fetchNijiVoice.js"
-import { getActorId } from "./lib/nijiVoice/nijiVoice.js"
+import { fetchNijiVoice } from "./lib/nijiVoice/fetchNijiVoice.js";
+import { getActorId } from "./lib/nijiVoice/actors.js";
 
 async function main(): Promise<void> {
   const response = await fetchNijiVoice({
     actorId: getActorId("春野 奏汰"),
     word: "こんにちは！",
     speed: 0.8,
-  })
+  });
 
-  console.log(response)
+  console.log(response);
 }
 
-main()
+main();
--- src/lib/nijiVoice/actors.ts
+++ src/lib/nijiVoice/actors.ts
@@ -7,16 +7,16 @@ const ACTORS = [
     name: "春野 奏汰",
     id: "0a1f77b7-db87-4c35-9e31-5c6155097124",
   },
-]
+];
 
-type ActorName = (typeof ACTORS)[number]["name"]
+type ActorName = (typeof ACTORS)[number]["name"];
 
 export function getActorId(name: ActorName): string {
-  const actor = ACTORS.find((actor) => actor.name === name)
+  const actor = ACTORS.find((actor) => actor.name === name);
 
   if (!actor) {
-    throw new Error(`Actor not found: ${name}`)
+    throw new Error(`Actor not found: ${name}`);
   }
 
-  return actor.id
+  return actor.id;
 }
